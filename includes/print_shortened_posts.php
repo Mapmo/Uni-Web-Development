c28f1d2392d6a9085e90484351fac3622ac172fa
Martin Iliev <mapmoeu@gmail.com>
2020-03-07 19:00:09 +0200

    Improve colouring in terms of consistency

--- includes/print_shortened_posts.php
+++ includes/print_shortened_posts.php
@@ -7,7 +7,7 @@ if(mysqli_num_rows($select_posts) > 0)
                 echo "<h2>".$row['title']."</h2>";
 		echo "<p class='authdate'><img src='img/user_icon.png' height='12' width='12'> ".$row['user']." (".$row['email'].")<span class='slash'> ||  <img src='img/date_icon.png' height='15' width='15'></span>  ".$row['date']."</p>";
 		echo "<p>".$row['summary']."</p>";
-                echo '<a href="../full_post.php?post_id='.$row['post_id'].'">read more</a>';
+                echo '<a href="../full_post.php?post_id='.$row['post_id'].'"><b>read more</b></a>';
                 echo '<p class="blogbottom">Category: <span class="tags"> <a href="search.php?cat='.$row['category'].'">'.$row['category'].'</a> <span class="slash">//</span> <a href="#">report</a></p>';
                 echo "</div>";
 	}
